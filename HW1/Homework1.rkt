460bc351ba99081ede18f25ee6c7e99c3274f89e
klemikiko <klemen@kotar.us>
2018-04-02 10:06:39 -0700

    Problem 4 in progress

--- HW1/Homework1.rkt
+++ HW1/Homework1.rkt
@@ -10,6 +10,7 @@
     list
     (cons (* (car list) (car list)) (squares (cdr list)))))
 
+;;; function that takes a list of numbers and returns a new list of the squares of those numbers
 (define (squares list)
   (if (null? list)
     list
@@ -17,12 +18,16 @@
          [tail (cdr list)])
       (cons (* head head) (squares tail)))))
 
+;;; function that takes a list of numbers and returns a new list of the squares of those numbers
+;;; using map and an anonymous function
 (define (map-squares list)
   (map (lambda (x) (* x x)) list))
 
+;;; non-tail-recursive function that tests whether a list of integers is in strict ascending order
 (define (ascending-1 list)
   (or (null? list) (eq? (length list) 1) (and (< (car list) (cadr list)) (ascending-1 (cdr list)))))
 
+;;; tail-recursive function that tests whether a list of integers is in strict ascending order
 (define (ascending list)
   (cond [(null? list) #t]
         [(eq? (length list) 1) #t]
@@ -30,6 +35,12 @@
         [else (ascending (cdr list))]))
 
 
+(define (let*->let expression)
+  (let ([variables (cadr expression)]
+        [tail (cddr expression)])
+    (if (null? variables)
+        tail
+        (cons (list 'let (car variables)) (let*->let (list (car expression) (cdr variables) tail))))))
 
 (provide squares
          map-squares
