4049f58866aa528b510c63b1d9268275dc8ffdb2
klemikiko <klemen@kotar.us>
2018-04-02 02:26:56 -0700

    Problems 2 & 3 solved + tested

--- HW1/Homework1-test.rkt
+++ HW1/Homework1-test.rkt
@@ -9,12 +9,31 @@
 (check-equal? (squares '(1 -3 15 0 -20)) '(1 9 225 0 400) "Multiple Element + Negative Numbers List")
 (test-case
  "List has decimal values that Squares rounds"
- (let ([output-list (squares '(0 -0.7 1.2))]
-       [anwser-list '(0 0.49 1.44)])
+ (let ([actual-list (squares '(0 -0.7 1.2))]
+       [expected-list '(0 0.49 1.44)])
        (for-each
-        (lambda (output anwser)
-          (check-= output anwser 0.000000001))
-        output-list
-        anwser-list)))
+        (lambda (actual expected)
+          (check-= actual expected 0.000000001))
+        actual-list
+        expected-list)))
 
 
+(check-equal? (map-squares '()) '() "Empty List")
+(check-equal? (map-squares '(1)) '(1) "Single Element List")
+(check-equal? (map-squares '(1 2)) '(1 4) "Two Element List")
+(check-equal? (map-squares '(1 -3 15 0 -20)) '(1 9 225 0 400) "Multiple Element + Negative Numbers List")
+(test-case
+ "List has decimal values that Squares rounds"
+ (let ([actual-list (map-squares '(0 -0.7 1.2))]
+       [expected-list '(0 0.49 1.44)])
+       (for-each
+        (lambda (actual expected)
+          (check-= actual expected 0.000000001))
+        actual-list
+        expected-list)))
+
+(check-eq? (ascending '()) #t "Empty List")
+(check-eq? (ascending '(42)) #t "Single Entry")
+(check-eq? (ascending '(1 3 5 7 1200)) #t "Ascending List")
+(check-eq? (ascending '(190 7 3 1)) #f "Strictly Descending")
+(check-eq? (ascending '(1 2 3 5 9 7)) #f "PArtially Ascending, Partially Descending")
--- HW1/Homework1.rkt
+++ HW1/Homework1.rkt
@@ -11,10 +11,26 @@
     (cons (* (car list) (car list)) (squares (cdr list)))))
 
 (define (squares list)
-  (if (eq? list '())
+  (if (null? list)
     list
     (let([head (car list)]
          [tail (cdr list)])
       (cons (* head head) (squares tail)))))
 
-(provide squares)
+(define (map-squares list)
+  (map (lambda (x) (* x x)) list))
+
+(define (ascending-1 list)
+  (or (null? list) (eq? (length list) 1) (and (< (car list) (cadr list)) (ascending-1 (cdr list)))))
+
+(define (ascending list)
+  (cond [(null? list) #t]
+        [(eq? (length list) 1) #t]
+        [(>= (car list) (cadr list)) #f]
+        [else (ascending (cdr list))]))
+
+
+
+(provide squares
+         map-squares
+         ascending)
